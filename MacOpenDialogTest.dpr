607cb32d80be6598148b80cc1dbd9de2aec83e5f
gkoehn2020 <59707158+gkoehn2020@users.noreply.github.com>
2021-11-17 18:02:02 -0800

    fixed up sample

---
+++ MacOpenDialogTest.dpr
@@ -0,0 +1,15 @@
+program MacOpenDialogTest;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  untVirtualUIInit in 'untVirtualUIInit.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ untVirtualUIInit.pas
@@ -0,0 +1,11 @@
+unit untVirtualUIInit;
+
+interface
+
+uses VirtualUI_SDK;
+
+implementation
+
+initialization
+  VirtualUI.Start();
+end.
